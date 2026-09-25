6a836edd7aab6b456110b9761f8372f9964a94c3
karthik-kulal8050 <karthikkulal8050@gmail.com>
2026-09-25 20:24:53 +0530

    API Change

--- app.py
+++ app.py
@@ -8,13 +8,10 @@ import plotly.express as px
 from ai.agent import ask_financial_analyst
 
 
-if "API_URL" in st.secrets:
-    API_URL = st.secrets["API_URL"]
-else:
-    API_URL = os.getenv(
-        "API_URL",
-        "http://127.0.0.1:8000"
-    )
+API_URL = os.getenv(
+    "API_URL",
+    "http://127.0.0.1:8000"
+)
 
 
 st.set_page_config(
